32bc1b421eceeba94b0df0a4dd46bd47bf0bc4b0
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-17 11:01:53 +0300

    updated: static variable and methods

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinoop2/Aclass.kt
@@ -0,0 +1,17 @@
+package com.ismailmesutmujde.kotlinoop2
+
+class Aclass {
+
+    var y = 20
+
+
+    companion object{
+        // static variable
+        var x = 10
+
+        // static method
+        fun method() {
+            println("Hello")
+        }
+    }
+}
---
+++ app/src/main/java/com/ismailmesutmujde/kotlinoop2/AclassMain.kt
@@ -0,0 +1,12 @@
+package com.ismailmesutmujde.kotlinoop2
+
+fun main() {
+
+    val a = Aclass()
+    println(a.y)
+
+    // static değişken ve metodlara sınıf ismiyle erişebiliyoruz.
+    println(Aclass.x)
+    println(Aclass.method())
+
+}
