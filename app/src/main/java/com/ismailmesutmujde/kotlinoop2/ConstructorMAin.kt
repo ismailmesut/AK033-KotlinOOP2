c274ebb390cd7d1ef073009992203b78d4421bee
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-17 10:26:54 +0300

    updated: constructor

---
+++ app/src/main/java/com/ismailmesutmujde/kotlinoop2/ConstructorMAin.kt
@@ -0,0 +1,7 @@
+package com.ismailmesutmujde.kotlinoop2
+
+fun main() {
+    val person = Person("ismail",18)
+    println(person.name)
+    println(person.age)
+}
---
+++ app/src/main/java/com/ismailmesutmujde/kotlinoop2/Person.kt
@@ -0,0 +1,7 @@
+package com.ismailmesutmujde.kotlinoop2
+
+class Person(var name : String, var age : Int) {
+    init {
+        println("Person object created.")
+    }
+}
