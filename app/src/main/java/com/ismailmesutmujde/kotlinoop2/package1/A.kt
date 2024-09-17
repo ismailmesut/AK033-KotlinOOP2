438df110357c89ed9384876d446684db2f830e5f
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-17 10:52:38 +0300

    updated: visibility modifiers (public, private, internal, protected)

--- app/src/main/java/com/ismailmesutmujde/kotlinoop2/package1/A.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinoop2/package1/A.kt
@@ -1,8 +1,21 @@
 package com.ismailmesutmujde.kotlinoop2.package1
 
-import com.ismailmesutmujde.kotlinoop2.package2.B
+open class A {
 
-class A {
+    var defaultVariable = 1
+    public var publicVariable = 2
+    private var privateVariable = 3
+    internal var internalVariable = 4
+    protected var protectedVariable = 5
 
-    val b = B()
+    // private değişkene ancak oluşturulduğu sınıfdan erişim yapabiliriz, başka sınıfdan erişemeyiz.
+
+    fun functionA() {
+        val a = A()
+        println(defaultVariable)
+        println(publicVariable)
+        println(privateVariable)
+        println(internalVariable)
+        println(protectedVariable)
+    }
 }
--- app/src/main/java/com/ismailmesutmujde/kotlinoop2/package2/B.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinoop2/package2/B.kt
@@ -1,4 +1,15 @@
 package com.ismailmesutmujde.kotlinoop2.package2
 
-class B {
+import com.ismailmesutmujde.kotlinoop2.package1.A
+
+class B : A() {
+
+    fun functionB() {
+        val a = A()
+        println(defaultVariable)
+        println(publicVariable)
+        //println(privateVariable)
+        println(internalVariable)
+        println(protectedVariable)
+    }
 }
